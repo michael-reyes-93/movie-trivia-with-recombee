afd0c652418780ad7371e09005a1c50e0d6615b8
Michael reyes <michaelreyesfernandez@hotmail.com>
2020-07-08 23:33:36 -0600

    footer

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,3 +1,14 @@
+  <!-- Footer -->
+  <footer class="page-footer font-small bg-secondary">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> Michael Reyes</a>
+  </div>
+  <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
   <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
